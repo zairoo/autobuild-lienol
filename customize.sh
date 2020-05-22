48167993e06f7dbf06cc0be4b29ca5dd1ce71ad5
zairoo <zairoo@qq.com>
2020-05-23 00:10:15 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.99/g' openwrt/package/base-files/files/bin/config_generate
